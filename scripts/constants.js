0e7513af6032a0cd0e1619bc1e0eac8647e5c14c
Andrey Salnikov <chijik666777@yandex.ru>
2022-09-29 16:23:14 +0300

    Mesto ПР7. Дополнительный рефакторинг и правки после ревью

--- scripts/constants.js
+++ scripts/constants.js
@@ -24,3 +24,12 @@ export const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
+
+export const settingValidation =
+  {
+    inputSelector: '.popup__info',
+    submitButtonSelector: '.popup__save-button',
+    inactiveButtonClass: 'popup__save-button_disabled',
+    inputErrorClass: 'popup__input_type_error',
+    errorClass: 'popup__input_error_visible'
+  }
--- scripts/index.js
+++ scripts/index.js
@@ -1,74 +1,54 @@
 import Card from '../scripts/Card.js';
-import FormValidator from "./FormValidator.js";
-import {initialCards} from "./constants.js";
+import {FormValidator} from "./FormValidator.js";
+import {initialCards, settingValidation} from "./constants.js";
 
-const settingValidation =
-  {
-  inputSelector: '.popup__info',
-  submitButtonSelector: '.popup__save-button',
-  inactiveButtonClass: 'popup__save-button_disabled',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__input_error_visible'
-  }
-const editButton = document.querySelector(".profile__edit-button");
+const photoGrid = document.querySelector(".photo-grid");
 const popupAddForm = document.querySelector("#popupAddForm");
 const popupForm = document.querySelector("#popupEditForm");
 const popupTitleImg = document.querySelector(".popup__info_text_title");
 const popupLinkImg = document.querySelector(".popup__info_text_link");
 const popupName = document.querySelector(".popup__info_text_name");
 const popupJob = document.querySelector(".popup__info_text_job");
-const profileTitle = document.querySelector(".profile__title");
-const profileSubtitle = document.querySelector(".profile__subtitle");
-const photoGrid = document.querySelector(".photo-grid");
-const addButton = document.querySelector(".profile__add-button");
 const popupEditProfile = document.querySelector("#popupEditProfile");
 const popupEditAdd = document.querySelector("#popupAddCard");
-const submitAddButton = document.querySelector("#submitAddButton");
+const popupsAll = document.querySelectorAll(".popup");
+const profileTitle = document.querySelector(".profile__title");
+const profileSubtitle = document.querySelector(".profile__subtitle");
+const buttonEdit = document.querySelector(".profile__edit-button");
+const buttonAdd = document.querySelector(".profile__add-button");
+const buttonSubmitAdd = document.querySelector("#submitAddButton");
+const FormValidateEdit = new FormValidator(settingValidation, '#popupEditForm');
+const FormValidateAdd = new FormValidator(settingValidation, '#popupAddForm');
 
 const settingProfileInfo = () => {
   popupName.value = profileTitle.textContent;
   popupJob.value = profileSubtitle.textContent;
 }
 
-function onValidation() {
-  const validateEditForm = new FormValidator(settingValidation, '#popupEditForm');
-  validateEditForm.enableValidation();
-  const validateAddForm = new FormValidator(settingValidation, '#popupAddForm');
-  validateAddForm.enableValidation();
-
-}
-settingProfileInfo();
-
-onValidation();
-
-
-function createCard(src, title) {
-  const newCard = new Card({src,title},'#photoGrid')
+function createCard(cardData) {
+  const newCard = new Card(cardData,'#photoGrid')
   return newCard.generateCard();
 }
 
-initialCards.forEach(renderCard);
-
-function renderCard(card) {
-   photoGrid.prepend(createCard(card.link, card.name));
+for (let i = 0;i < initialCards.length;i++) {
+  renderCard(initialCards[i],photoGrid)
 }
 
-function saveProfInf(name, job) {
+function renderCard(card,template) {
+   template.prepend(createCard(card));
+}
+
+function saveProfileInformation(name, job) {
   profileTitle.textContent = name.value;
   profileSubtitle.textContent = job.value;
   closePopup(popupEditProfile);
 }
 
-const offButton = (button) => {
-  button.classList.add('popup__save-button_disabled');
-  button.disabled = true;
-}
-
-function addPopupImg(name, link) {
+function addPopupImg(link, name) {
   renderCard({
-    link: name.value, name: link.value,
-  });
-  offButton(submitAddButton);
+    link: link.value, name: name.value,
+  },photoGrid);
+  FormValidateAdd.disableSubmitButton(buttonSubmitAdd);
   closePopup(popupEditAdd);
   popupAddForm.reset();
 }
@@ -79,6 +59,10 @@ const closePopupOverlay = (event) => {
   }
 }
 
+popupsAll.forEach((el) => {
+  el.addEventListener("mousedown", closePopupOverlay);
+})
+
 const closePopupEsc = (event) => {
   if (event.key === "Escape") {
     const openedPopup = document.querySelector('.popup_opened');
@@ -86,30 +70,37 @@ const closePopupEsc = (event) => {
   }
 }
 
-const openProfilePopup = (modalWindow) => {
-  popupName.value = profileTitle.textContent;
-  popupJob.value = profileSubtitle.textContent;
-  openPopup(modalWindow);
+const openProfilePopup = () => {
+  settingProfileInfo();
+  openPopup(popupEditProfile);
 }
 
- const openAddPopup = (modalWindow) => {
+ const openAddPopup = () => {
   popupAddForm.reset();
-  openPopup(modalWindow);
+  openPopup(popupEditAdd);
 }
 
 export function openPopup(modalWindow) {
   modalWindow.classList.add("popup_opened");
   document.addEventListener("keydown", closePopupEsc);
-  modalWindow.addEventListener("mousedown", closePopupOverlay);
 }
 
 function closePopup(modalWindow) {
   modalWindow.classList.remove("popup_opened");
   document.removeEventListener("keydown", closePopupEsc);
-  modalWindow.removeEventListener("mousedown", closePopupOverlay);
 }
 
-editButton.addEventListener("click", () => openProfilePopup(popupEditProfile));
-addButton.addEventListener("click", () => openAddPopup(popupEditAdd));
-popupForm.addEventListener("submit", () => saveProfInf(popupName, popupJob));
+buttonEdit.addEventListener("click", () => openProfilePopup(popupEditProfile));
+buttonAdd.addEventListener("click", () => openAddPopup(popupEditAdd));
+popupForm.addEventListener("submit", () => saveProfileInformation(popupName, popupJob));
 popupAddForm.addEventListener("submit", () => addPopupImg(popupLinkImg, popupTitleImg));
+
+//В прошлом спринте было сказано, что при открытии попаппа с редакитированием профиля, если он заполнен,
+// то кнопка должна быть активной и работать.По умолчанию он заполняется только при открытии профиля,
+// а валидация и сама проверка на заполненность полей начинает работать ещё до его открытия
+// и поэтому при первом открытии кнопка не активна, хотя данные в полях есть,
+// но если это ошибка, то в следущей итерации я уберу отсюда эту функцию
+settingProfileInfo();
+
+FormValidateEdit.enableValidation();
+FormValidateAdd.enableValidation();
